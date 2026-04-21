a1e187a1764742d580a59cf384f9259e6e731c7f
Ivan K <54704529+VizzleTF@users.noreply.github.com>
2026-04-21 17:39:08 +0300

    fix: serve Yandex verification via Pages Function to avoid Clean-URL 308 (#7)

--- functions/yandex_8b33934bab1533e3.html.js
+++ functions/yandex_8b33934bab1533e3.html.js
@@ -1,6 +1,10 @@
-<html>
+export const onRequestGet = () =>
+  new Response(
+    `<html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>Verification: 8b33934bab1533e3</body>
-</html>
+</html>`,
+    { headers: { "Content-Type": "text/html; charset=UTF-8" } }
+  )
