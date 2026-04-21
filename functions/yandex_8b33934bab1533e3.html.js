a058df84775d2fa952f7b2dc15efab7bf74e3a2d
Ivan K <vizzlef@gmail.com>
2026-04-21 17:41:27 +0300

    fix: add trailing newline to Yandex verification body

--- functions/yandex_8b33934bab1533e3.html.js
+++ functions/yandex_8b33934bab1533e3.html.js
@@ -5,6 +5,7 @@ export const onRequestGet = () =>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>Verification: 8b33934bab1533e3</body>
-</html>`,
+</html>
+`,
     { headers: { "Content-Type": "text/html; charset=UTF-8" } }
   )
